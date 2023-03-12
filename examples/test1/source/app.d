d4d559dca95579a0eed4771817f4792066ea1ff6
aferust <aferust@gmail.com>
2023-03-13 00:02:37 +0300

    apples cocoa

--- examples/test1/source/app.d
+++ examples/test1/source/app.d
@@ -6,8 +6,13 @@ import bindbc.cocoa;
 
 void main(){
     
-    auto _rt = loadRT("D:/developer_tools/GNUstep/x64/Release/bin/objc.dll"); writeln(_rt);
-    auto _fo = loadFoundation("D:/developer_tools/GNUstep/x64/Release/bin/gnustep-base-1_28.dll"); writeln(_fo);
+    // with gnustep
+    //auto _rt = loadRT("D:/developer_tools/GNUstep/x64/Release/bin/objc.dll"); writeln(_rt);
+    //auto _fo = loadFoundation("D:/developer_tools/GNUstep/x64/Release/bin/gnustep-base-1_28.dll"); writeln(_fo);
+
+    // with Apple's cocoa
+    auto _rt = loadRT("C:/Program Files/iTunes/objc.dll"); writeln(_rt);
+    auto _fo = loadFoundation("C:/Program Files/iTunes/Foundation.dll"); writeln(_fo);
 
     NSLog(NSString.stringWith("Hello from Cocoa, here is some number: %d\n"w)._id, 3);
 }
