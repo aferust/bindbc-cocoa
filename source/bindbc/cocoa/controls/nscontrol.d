3f4fd17fb5cf9b7a051d575d9a9cea8d95502c6d
aferust <aferust@gmail.com>
2023-03-23 00:16:15 +0300

    fix setAction

--- source/bindbc/cocoa/controls/nscontrol.d
+++ source/bindbc/cocoa/controls/nscontrol.d
@@ -12,11 +12,31 @@ nothrow @nogc:
 
     mixin NSObjectTemplate!(NSControl, "NSControl");
 
-    void setAction(void* fun){
-        // https://stackoverflow.com/questions/22570842/c-function-pointer-to-sel
-        class_addMethod(getClass, sel!"myNewAction", cast(IMP)fun, "v@:@");
+    void setAction(void function(id, SEL, ...) fun){
+        
+        import core.stdc.stdio;
+        
+        char* _cp = cast(char*)_id;
+
+        char[32] m_name;
+        sprintf(m_name.ptr, "%p_action", _cp);
+
+        SEL _sel = varsel_registerName(m_name.ptr);
+
+        class_addMethod(_id.isa, _sel, cast(IMP)fun, "v@:i@");
         alias fun_t = extern(C) void function (id, SEL, SEL) nothrow @nogc;
-        (cast(fun_t)objc_msgSend)(_id, sel!"setAction:", sel!"myNewAction");
+        (cast(fun_t)objc_msgSend)(_id, sel!"setAction:", _sel);
+    }
+
+    void setTag(NSInteger anInt){
+
+        alias fun_t = extern(C) void function (id, SEL, NSInteger) nothrow @nogc;
+        (cast(fun_t)objc_msgSend)(_id, sel!"setTag:", anInt);
+    }
+
+    NSInteger tag(){
+        alias fun_t = extern(C) NSInteger function (id, SEL) nothrow @nogc;
+        return (cast(fun_t)objc_msgSend)(_id, sel!"tag");
     }
 }
 
